1d0d86e9374f458cd9059731dd0ca29df5deb104
Victor Cortes <ie.cortex@gmail.com>
2013-07-16 18:29:32 -0500

    Read index.html and pass to a buffer

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var indexContent = fs.readFileSync('index.html');
+	var buffer = new Buffer(indexContent, "utf-8")
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
